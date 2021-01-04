62cc0becc2ea53035f9b65e737c028205578e2e6
Ved Prakash <vedp@nihilentanalytics.com>
2021-01-04 13:44:45 +0530

    first

--- actions/Fetch.py
+++ actions/Fetch.py
@@ -1,14 +1,14 @@
 import tweepy
 from st2common.runners.base_action import Action
 
-'''consumer_key = ""
+consumer_key = ""
 consumer_secret = ""
 access_key= ""
 access_secret = ""
 username = ""
-#user_id=786601883691515904'''
+#user_id=786601883691515904
  
-consumer_key = "AyeMkToNVdtICkcXSynim0ASn"
+'''consumer_key = "AyeMkToNVdtICkcXSynim0ASn"
 consumer_secret = "ec4ffvb8SLaC79U080hyqWR0oqxWmcNM7iollCuoTrikIfhIbs"
 access_key= "786601883691515904-ujfS8pA1yvVm7pNpjNL5qM31JaXnZGq"
 access_secret = "3B1tlDerb7EAeF3NdFVcrr3vqWifiX8gzsLYS8h0V6Tdz"
@@ -16,13 +16,13 @@ access_secret = "3B1tlDerb7EAeF3NdFVcrr3vqWifiX8gzsLYS8h0V6Tdz"
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
 api = tweepy.API(auth,wait_on_rate_limit=True)
-
+'''
 class Myaction(Action):
 
-    def run(self, username):
-        '''auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    def run(self, username, consumer_key, consumer_secret, access_key, access_secret):
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
         auth.set_access_token(access_key, access_secret)
-        api = tweepy.API(auth,wait_on_rate_limit=True)'''
+        api = tweepy.API(auth,wait_on_rate_limit=True)
         for Tweet in api.user_timeline(username):
             text = str(Tweet.text.encode("utf-8"))
             if "Stackstorm" not in text:
--- actions/send.py
+++ actions/send.py
@@ -3,8 +3,13 @@ import json
 import requests
 from st2common.runners.base_action import Action
 
+consumer_key = ""
+consumer_secret = ""
+access_key= ""
+access_secret = ""
+username = ""
 
-consumer_key = "AyeMkToNVdtICkcXSynim0ASn"
+'''consumer_key = "AyeMkToNVdtICkcXSynim0ASn"
 consumer_secret = "ec4ffvb8SLaC79U080hyqWR0oqxWmcNM7iollCuoTrikIfhIbs"
 access_key= "786601883691515904-ujfS8pA1yvVm7pNpjNL5qM31JaXnZGq"
 access_secret = "3B1tlDerb7EAeF3NdFVcrr3vqWifiX8gzsLYS8h0V6Tdz"
@@ -13,10 +18,13 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
 api = tweepy.API(auth,wait_on_rate_limit=True)
 
-username = "@vedprakash531"
+username = "@vedprakash531"'''
 class Myaction(Action):
 
-    def run(self, username):
+    def run(self, username, consumer_key, consumer_secret, access_key, access_secret):
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_key, access_secret)
+        api = tweepy.API(auth,wait_on_rate_limit=True)
         for Tweet in api.user_timeline(username):
             text = str(Tweet.text.encode("utf-8"))
             if "Stackstorm" not in text:
