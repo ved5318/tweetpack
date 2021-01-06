6cfc3d990941bbdee2bac56e0556d0431b1001e7
Ved Prakash <vedp@nihilentanalytics.com>
2021-01-06 12:18:05 +0530

    final

--- actions/send.py
+++ actions/send.py
@@ -4,22 +4,7 @@ import requests
 from Fetch import text
 from st2common.runners.base_action import Action
 
-'''consumer_key = ""
-consumer_secret = ""
-access_key= ""
-access_secret = ""
-username = ""'''
-
-'''consumer_key = "AyeMkToNVdtICkcXSynim0ASn"
-consumer_secret = "ec4ffvb8SLaC79U080hyqWR0oqxWmcNM7iollCuoTrikIfhIbs"
-access_key= "786601883691515904-ujfS8pA1yvVm7pNpjNL5qM31JaXnZGq"
-access_secret = "3B1tlDerb7EAeF3NdFVcrr3vqWifiX8gzsLYS8h0V6Tdz"
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_key, access_secret)
-api = tweepy.API(auth,wait_on_rate_limit=True)
-
-username = "@vedprakash531"'''
+username = "@vedprakash531"
 class Myaction(Action):
 
     def run(self):
@@ -27,14 +12,7 @@ class Myaction(Action):
         payload = {"text": text }
         r = requests.post(url, data=json.dumps(payload))
         print(r.text)
-        return(True, r.text)
-        '''auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-        auth.set_access_token(access_key, access_secret)
-        api = tweepy.API(auth,wait_on_rate_limit=True)
-        for Tweet in api.user_timeline(username):
-            text = str(Tweet.text.encode("utf-8"))
-            if "Stackstorm" not in text:
-                continue'''    
+        return(True, r.text)   
             
 #print(run(username))
 '''def notify_slack(text):
