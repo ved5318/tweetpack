5ec24ae7a587489fbb631b415c6978be22b39046
Ved Prakash <vedp@nihilentanalytics.com>
2021-01-08 16:29:43 +0530

    final

--- actions/add.py
+++ actions/add.py
@@ -3,7 +3,7 @@ from st2common.runners.base_action import Action
 
 class MyAction(Action):
 
-    def run(self):
-        c = 3 + 4
+    def run(self, a, b):
+        c = int(a) + int(b)
         print(c)
         return(True,c)
