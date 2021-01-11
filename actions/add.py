33236d4a755d2efc8c5ff1716b6964ac9ca154d6
Ved Prakash <vedp@nihilentanalytics.com>
2021-01-11 10:38:27 +0530

    first

--- actions/add.py
+++
@@ -1,9 +0,0 @@
-import sys
-from st2common.runners.base_action import Action
-
-class MyAction(Action):
-
-    def run(self, a, b):
-        c = a + b
-        print(c)
-        return {"result": str(c)}
