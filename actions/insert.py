bca0ae517e8164a86abfeae5c8fa3e8791e7594b
Ved Prakash <vedp@nihilentanalytics.com>
2021-01-25 15:57:07 +0530

    first

--- actions/insert.py
+++ actions/insert.py
@@ -1,21 +1,15 @@
 import mysql.connector
 from st2common.runners.base_action import Action
 
-# mydb = mysql.connector.connect(
-#   host="localhost",
-#   user="root",
-#   password="",
-#   database="practice"
-# )
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="practice"
+)
 class insertdb(Action):
 
-    def run(self, hostname, username, passwd, dbname):
-        mydb = mysql.connector.connect(
-            host= hostname,
-            user= username,
-            password= passwd,
-            database= dbname
-        )
+    def run(self):
         mycursor = mydb.cursor()
         sql = "INSERT INTO Info (e_name, e_id, e_add) VALUES (%s, %s, %s)"
         val = ("ved", 2, "Newtown")
