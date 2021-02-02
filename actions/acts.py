b2d18e6744e93b2a3e251f41727110095508feea
Ved Prakash <vedp@nihilentanalytics.com>
2021-02-02 12:32:23 +0530

    first

--- actions/acts.py
+++ actions/acts.py
@@ -1,29 +1,48 @@
-from lib.action import St2BaseAction
-from lib.formatters import format_client_list_result
+import requests 
+import json
+import base64
+import urllib3
 
-__all__ = [
-    'St2ActionsListAction'
-]
+class sActs(Action):
+
+    def run(self):
+        URL = "https://192.168.9.174/auth/v1/tokens"
+ 
+        user = 'st2admin'
+        password = 'M1cr0123'
+        usrPass = user+":"+password
+        endcoded_u = base64.b64encode(usrPass.encode('utf-8'))
+        #base64encodedData = new Buffer(user + ':' + password).toString('base64')
+        headers = {
+        'Authorization' : "Basic "+str(endcoded_u.decode('UTF-8'))
+        }
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        r = requests.post(url = URL , headers=headers, verify=False)
+        res=r.text
+        res1=json.loads(res)
+        token = res1['token']
+
+        url ="https://192.168.9.174/api/v1/actions"
+        #base64encodedData = new Buffer(user + ':' + password).toString('base64')
+        endcoded_u = base64.b64encode(token.encode('utf-8'))
+        # #base64encodedData = new Buffer(user + ':' + password).toString('base64')
+        headers = {
+        'Connection': 'keep-alive',
+        'User-Agent': 'python-requests/2.9.1',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept': '*/*',
+        'X-Auth-Token': token
+        }
+        # headers={'Content-Type':'application/json',
+        #                'Authorization': 'Bearer {}'.format(token)}
+        # header = {'Authorization': 'Bearer aa83dc33de6347ee990ca1ba2cd7cdf3' }
+        result = requests.get(url,headers=headers, verify = False)
+        res2=result.text
+        res3=json.loads(res2)
+        for i in range(243):
+            print (res3[i]['name'])
+        return True
 
 
-def format_result(result, exclude_attributes):
-    return format_client_list_result(result=result, exclude_attributes=exclude_attributes)
 
 
-class St2ActionsListAction(St2BaseAction):
-    def run(self, pack=None, limit=None, exclude=None):
-        kwargs = {}
-
-        if limit:
-            kwargs['limit'] = limit
-
-        if pack:
-            kwargs['pack'] = pack
-
-        result = self._run_client_method(method=self.client.actions.get_all,
-                                         method_kwargs=kwargs,
-                                         format_func=format_result,
-                                         format_kwargs={
-                                             'exclude_attributes': exclude
-                                         })
-        return result
